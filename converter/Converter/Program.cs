f385a2fd0d06ba957efae42817838490b6293e70
olt egor <olt.egor@gmail.com>
2018-05-02 18:49:18 +0300

    db connection

--- converter/Converter/Program.cs
+++ converter/Converter/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 
 namespace Converter
 {
@@ -7,6 +8,30 @@ namespace Converter
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            using (var conn = new MySqlConnection("server=localhost;port=3306;database=test-ideo-q;user=root;SslMode=none;"))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from wp_posts", conn);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        /*
+                        list.Add(new Album()
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            Name = reader["Name"].ToString(),
+                            ArtistName = reader["ArtistName"].ToString(),
+                            Price = Convert.ToInt32(reader["Price"]),
+                            Genre = reader["genre"].ToString()
+                        });*/
+                    }
+                }
+            }
         }
+
+       
     }
 }
