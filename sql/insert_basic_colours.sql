d85abe53db01cc802f9042e27ab502d944fe05a6
olt egor <olt.egor@gmail.com>
2018-05-02 18:35:08 +0300

    first commit

---
+++ sql/insert_basic_colours.sql
@@ -0,0 +1,8 @@
+INSERT INTO `wp_terms` (`name`, `slug`, `term_group`) VALUES ('black', 'black', 0);
+INSERT INTO `wp_terms` (`name`, `slug`, `term_group`) VALUES ('white', 'white', 0);
+INSERT INTO `wp_terms` (`name`, `slug`, `term_group`) VALUES ('grey', 'grey', 0);
+INSERT INTO `wp_terms` (`name`, `slug`, `term_group`) VALUES ('blue', 'blue', 0);
+INSERT INTO `wp_terms` (`name`, `slug`, `term_group`) VALUES ('brown', 'brown', 0);
+INSERT INTO `wp_terms` (`name`, `slug`, `term_group`) VALUES ('red', 'red', 0);
+INSERT INTO `wp_terms` (`name`, `slug`, `term_group`) VALUES ('green', 'green', 0);
+INSERT INTO `wp_terms` (`name`, `slug`, `term_group`) VALUES ('silver', 'silver', 0);
